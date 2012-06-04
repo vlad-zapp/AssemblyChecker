1dcc19afda917b32c14147067617e859634ed9e6
Vladislav Grutsenko <vgrutsenko@agilent.com>
2012-06-04 17:14:12 +0400

    viewer wip

--- checker/ReportViewer/AsmCheckerNode.cs
+++ checker/ReportViewer/AsmCheckerNode.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Xml.Linq;
 
 namespace AsmChecker.ReportViewer
@@ -14,9 +16,29 @@ namespace AsmChecker.ReportViewer
 		public string Type { get; set; }
 		public string Name { get; set; }
 
+		private static Dictionary<string, BitmapImage> Images;
+		public static DataTemplate AllHeadersTemplate;
+
+		static AsmCheckerNode()
+		{
+			Images = new Dictionary<string, BitmapImage>();
+			Images.Add("CompatibilityInfo", new BitmapImage(new Uri(@"pack://Application:,,/Images/Info.png")));
+			Images.Add("Assembly", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Assembly.png")));
+			Images.Add("Class", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Class.png")));
+			Images.Add("Enum", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Enum.png")));
+			Images.Add("Field", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Field.png")));
+			Images.Add("Interface", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Interface.png")));
+			Images.Add("Method", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Method.png")));
+			Images.Add("Acessor", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Method.png")));
+			Images.Add("Property", new BitmapImage(new Uri(@"pack://Application:,,/Images/VSOBJECT_Properties.png")));
+		}
+
 		public AsmCheckerNode(XElement source)
 		{
-			Name = source.GetValue("Name");
+			Name = source.Attribute("Name") != null ? source.Attribute("Name").Value : "Compatibility Info";
+
+			HeaderTemplate = AllHeadersTemplate;
+			
 			Type = source.Name.LocalName;
 
 			bool comp;
@@ -28,6 +50,12 @@ namespace AsmChecker.ReportViewer
 			{
 				Compatible = null;
 			}
+
+			Header = new
+				{
+					Name = Name,
+					TypeImage = Images[Type]
+				};
 		}
 	}
 }
--- checker/ReportViewer/MainWindow.xaml.cs
+++ checker/ReportViewer/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Xml.Linq;
 using System.Linq;
 
@@ -11,13 +15,22 @@ namespace AsmChecker.ReportViewer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		static DataTemplate HeaderTemplate;
+
 		public MainWindow(string filePath)
 		{
 			InitializeComponent();
+			AsmCheckerNode.AllHeadersTemplate = FindResource("header_template") as DataTemplate;
 			try
 			{
-				BuildTree(treeView1, XElement.Load(filePath));
-				treeView1.Items.Add("asd");
+				XElement xml = XElement.Load(filePath);
+
+				Dump.ApplyPatch(xml, XElement.Load(Path.ChangeExtension(filePath,null)+"-report.xml"));
+
+				var node = new AsmCheckerNode(xml);
+				treeView1.Items.Add(node);
+
+				BuildTree(xml,node);
 			}
 			catch (Exception e)
 			{
@@ -26,9 +39,14 @@ namespace AsmChecker.ReportViewer
 			}
 		}
 
-		private void BuildTree(TreeView treeView, XElement xml)
+		private void BuildTree(XElement xml, AsmCheckerNode parentNode)
 		{
-			xml.Elements().Select(e => treeView.Items.Add(e.ToString()));
+			foreach (XElement e in xml.Elements().Where(e=>e.Name!="Parameter"))
+			{
+				AsmCheckerNode node = new AsmCheckerNode(e);
+				BuildTree(e, node);
+				parentNode.Items.Add(node);
+			}
 		}
 	}
 }
